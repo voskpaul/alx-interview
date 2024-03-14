8bced48a875b05aa1385817a8a4055e877eee42b
voskpaul <paulriemii@gmail.com>
2024-03-14 14:56:44 -0700

    updates

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,7 +1,31 @@
 #!/usr/bin/python3
-"""The module that establishes if all lockboxes can be unlocked"""
+"""
+This module provides a function to determine if all lockboxes can be unlocked.
+
+The function canUnlockAll checks if all lockboxes in a list of boxes can be opened
+by recursively searching for keys in the boxes and updating a list of open boxes.
+
+Args:
+    boxes (list): A list of lists representing the lockboxes and their corresponding keys.
+
+Returns:
+    bool: True if all lockboxes can be opened, False otherwise.
+
+Example:
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))  # Output: True
+"""
 
 def canUnlockAll(boxes):
+    """
+    Determine if all lockboxes can be unlocked.
+
+    Args:
+        boxes (list): A list of lists representing the lockboxes and their corresponding keys.
+
+    Returns:
+        bool: True if all lockboxes can be opened, False otherwise.
+    """
     if not boxes:
         return False
     box_len = len(boxes)
