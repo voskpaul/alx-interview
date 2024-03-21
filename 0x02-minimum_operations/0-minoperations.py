2f5397ac84d60a2ff7d8534cc312639b1357a363
voskpaul <paulriemii@gmail.com>
2024-03-21 10:57:16 -0700

    minimum_operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,37 @@
+
+#!/usr/bin/python3
+'''Interview Q2- Coding challeng: The minimum operations
+'''
+
+
+def minOperations(n):
+    '''s calculates the least no. of calculations needed for it to
+    result to n H characters.
+    '''
+    if not isinstance(n, int):
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
+
