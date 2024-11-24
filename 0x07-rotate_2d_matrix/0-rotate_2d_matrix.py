1c608bbb558372c3fe8daf2f8a7851d3d0eefa05
voskpaul <paulriemii@gmail.com>
2024-11-24 14:02:19 -0800

    Initial commit

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-'''2D matrix'''
+"""
+Define a function that rotates an nxn 2D matrix 90 degrees clockwise in-place
+"""
 
 
 def rotate_2d_matrix(matrix):
-    '''rotates a 2d matrix 90° clockwise
-    Returns: Nothing'''
-    left, right = 0, len(matrix) - 1
+    """
+    Rotate a 2d square matrix 90 degrees clockwise in-place
+    Args:
+        matrix (list): 2d square matrix
+    Return:
+        None
+    """
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
 
-    while left < right:
-        for i in range(right - left):
-            top, bottom = left, right
-            # save topleft  value
-            topLeft = matrix[top][left + i]
-            # move bottom left to top left
-            matrix[top][left + i] = matrix[bottom - i][left]
-            # move bottom right to bottom left
-            matrix[bottom - i][left] = matrix[bottom][right - i]
-            # move top right to bottom right
-            matrix[bottom][right - i] = matrix[top + i][right]
-            # move top left to top right
-            matrix[top + i][right] = topLeft
-        right -= 1
-        left += 1
-
+    for i in range(n):
+        for j in range(int(n / 2)):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][n-1-j]
+            matrix[i][n-1-j] = temp
